ed7c00438e9c066c58fe702513bcbba77b1642c2
azizbenzina <benzinaaziz1998@gmail.com>
2022-05-12 04:16:51 +0100

    4th commit

---
+++ react-component/src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,13 @@
+// import ImageInSrc from "./imageInSrc.png"
+const ProfilePhoto = () => {
+
+    return (  
+
+<div className="ImageInSrc">
+<img src="imageInSrc.png" alt="ProfilePhoto"/>
+    </div>
+     
+    );
+}
+ 
+export default ProfilePhoto ;
